222c781b8df853f4251db6523ee0be0f23566ab4
placu97 <matteogplacucci@gmail.com>
2023-06-20 20:00:56 +0200

    rimesso v3.1.1

--- src/main.tsx
+++ src/main.tsx
@@ -12,5 +12,5 @@ if ('serviceWorker' in navigator) {
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
+  </React.StrictMode>,//oooo
 )
