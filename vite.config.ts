e858839e35ed8a918ec7bcbe59fabe5d4e3fa8e1
placu97 <matteogplacucci@gmail.com>
2023-06-20 19:44:26 +0200

    tolto pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,31 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    VitePWA({
-      registerType: 'autoUpdate',
-      includeAssets: ['favicon.ico', 'robots.txt', 'apple-touch-icon.png', 'icons/*'],
-      manifest: {
-        name: 'My App',
-        short_name: 'My App',
-        theme_color: '#ffffff',
-        icons: [
-          {
-            src: '/icons/192x192.png',
-            sizes: '192x192',
-            type: 'image/png',
-          },
-          {
-            src: '/icons/512x512.png',
-            sizes: '512x512',
-            type: 'image/png',
-          },
-        ],
-      },
-    }),
+   
   ],
 });
